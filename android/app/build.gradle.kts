52d18ad2f073552b0affe07d78745bf8591bae8e
michioxd <michio.haiyaku@gmail.com>
2026-09-15 19:32:37 +0700

    bump 0.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ch.michioxd.sensorithm"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
